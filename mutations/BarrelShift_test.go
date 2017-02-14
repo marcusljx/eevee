6418c25f07852551741ccc625d56088d42999693
marcusljx <marcusljx@gmail.com>
2017-02-14 23:47:38 +0800

    Added unit tests

--- mutations/BarrelShift_test.go
+++ mutations/BarrelShift_test.go
@@ -12,7 +12,7 @@ func TestBarrelShiftMutation_Do(t *testing.T) {
 	rand.Seed(time.Now().Unix())
 	b := entities.NewSimpleTextEntity("HELLOWORLD")
 
-	c := NewBarrelShiftMutation(1.0, 1, 5, 7)
+	c := NewBarrelShiftMutation(1.0, 1, 3, 5, 7)
 	c.Do(b)
 	t.Log(b)
 }
